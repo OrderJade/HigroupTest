a7b82d0c9a07918d0b849a510a639f6a2e60165c
OrderJade <3229713727@qq.com>
2024-12-17 19:19:32 +0800

    problem A

--- problem.py
+++ problem.py
@@ -1,10 +1,3 @@
-import torch
-
-
-device = torch.device('cuda')  # 设置为 CUDA 设备
-x = torch.randn(3, 3).to(device)  # 创建一个 tensor 并移到 GPU 上
-print("CUDA is available, operation successful!")
-
-"""
-请你创建一个大小为 torch.size([1,1,3]) 的tensor, 并且将该tensor 拼接在x 的后面，形成一个大小为 torch.size([1,4,3]) 的tensor
-"""
+'''
+请使用torch 来计算y = 2*x^7 - 3*x^6 + 2*x^5 - 4*x + 10的一组极小值和极大值
+'''
